a66a305acbaca7754005f0be3b22bd21ea8b1a99
Drew Williamson <dreww@vt.edu>
2021-07-19 19:48:12 -0400

    Added UI components for home (which should be the destination for scanning a qr code) and user profile.
    Needs to implement back end.  Also, needs more responsiveness.

--- client/src/components/home/index.jsx
+++ client/src/components/home/index.jsx
@@ -1,9 +1,97 @@
-import React from 'react'
+import React from 'react';
+import styles from './Home.module.css'
 
 function Home() {
     return (
-        <div>Home</div>
+        <div className={styles.containerDiv}>
+            <LocationInfo/>
+
+            {/* TODO: Make this have conditional visibility based on number of actions */}
+            {/* Change this to see Multi-Action or Single-Action View*/}
+            <SingleAction/>
+
+        </div>
+    )
+}
+
+// This will be the one action option
+function SingleAction() {
+    return (
+        <div className={styles.actionContainer}>
+            <br/>
+            <h3 style={{textAlign: 'center'}}>What Should We Do?</h3>
+            <br/>
+            <div style={{width: '100%'}}>
+                <div className={styles.selection}>
+                    <div style={{height: 15}}/>
+                    <p style={{textAlign: 'center'}}>Option 1</p>
+                </div>
+                <br/>
+                <div className={styles.selection}>
+                    <div style={{height: 15}}/>
+                    <p style={{textAlign: 'center'}}>Option 2</p>
+                </div>
+                <br/>
+                <div className={styles.selection}>
+                    <div style={{height: 15}}/>
+                    <p style={{textAlign: 'center'}}>Option 3</p>
+                </div>
+            </div>
+            <div className={styles.buttonContainer}>
+                <p style={{paddingTop: 10, marginRight: 10}}>4 Votes Left</p>
+                <button className="ui primary button">
+                    Submit
+                </button>
+            </div>
+        </div>
+
+    )
+}
+
+// This will be the multi action option
+function MultiAction() {
+    return (
+
+        <div className={styles.actionContainer}>
+            <div style={{width: '100%', marginTop: 80}}>
+                <div className={styles.selection}>
+                    <div style={{height: 15}}/>
+                    <p style={{textAlign: 'center'}}>Pick a Beverage >></p>
+                </div>
+                <br/>
+                <div className={styles.selection}>
+                    <div style={{height: 15}}/>
+                    <p style={{textAlign: 'center'}}>Pick Next Month's Special >></p>
+                </div>
+                <br/>
+                <div className={styles.selection}>
+                    <div style={{height: 15}}/>
+                    <p style={{textAlign: 'center'}}>Enter Raffle >></p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function LocationInfo() {
+    return (
+        <div className={styles.actionContainer}>
+            <br/>
+            <h2 style={{textAlign: 'center'}}>Restaurant Title</h2>
+            <div style={{width: '80%', height: 155, background: 'gray', marginLeft: '10%'}}>
+                <p>Insert Image Here</p>
+            </div>
+            <br/>
+            <p style={{padding: 5, textAlign: 'center'}}>
+                This paragraph is to describe a given campaign / offer etc. It will outline what it wants to
+                accomplish along with any kind of other information that needs to be conveyed.
+            </p>
+
+        </div>
     )
 }
 
 export default Home
+
+// TODO: generally we need to figure out how we are differentiating clients and users.  We also need
+//  to figure out how we are storing a client's campaigns
--- client/src/components/profile/index.js
+++ client/src/components/profile/index.js
@@ -1,14 +1,96 @@
 import React, {useState, useEffect} from 'react';
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
+import styles from './profile.module.css'
+
 function Profile(props) {
     return (
-        <div>
-            <h1>Profile</h1>
-            <p>{props.auth.user.name}</p>
+        <div className={styles.profileContainer}>
+            <EndUserDashboard props={props} /> {/* TODO: Figure out how to pass through props easier */}
+            {/* TODO: Make client Dashboard, make switch with profile type */}
         </div>
     )
 }
+
+function EndUserDashboard(props) {
+    return (
+        <div>
+            <br/>
+            <h1 style={{textAlign: "center"}}>Welcome, {props.props.auth.user.name}!</h1>
+            <div className={styles.endUserDashboardContainer}>
+                <AccountSettings/>
+                <History/>
+            </div>
+        </div>
+    )
+}
+
+function AccountSettings() {
+    // TODO: Make these inputs update user in backend
+    return (
+        <div className={styles.userAccountSettings}>
+            <div style={{backgroundColor: 'gray', width: 200, height: 150, borderRadius: 10}}>
+                Profile Pic
+            </div>
+            <br/>
+            <div className="ui input" style={{marginBottom: 10}}>
+                <input type="text" placeholder="Email" required/>
+            </div>
+
+            <div className="ui input" style={{marginBottom: 10}}>
+                <input type="text" placeholder="Username" required/>
+            </div>
+
+            <div className="ui input">
+                <input type="text" placeholder="Password" required/>
+            </div>
+            <br/>
+            <div>
+                {/* TODO: Make Buttons work, reset fields */}
+                <button className="ui button">
+                    Cancel
+                </button>
+                <button className="ui primary button">
+                    Submit
+                </button>
+            </div>
+        </div>
+    )
+}
+
+function History() {
+
+    // TODO: Figure out what exactly we want to report here.  What information do we need to display?
+    return (
+        <div className={styles.userAccountSettings}>
+            <h2>History</h2>
+            <br/>
+            <div className={styles.divider}/>
+            <br/>
+            <LocationHistory name='Restaurant 1' />
+            <br/>
+            <div className={styles.divider}/>
+            <br/>
+            <LocationHistory name='Restaurant 2' />
+        </div>
+    )
+}
+
+/**
+ * Made Location History different because it will be used multiple times.  Takes in name as prop
+ */
+function LocationHistory(props) {
+    // TODO: Figure out how history will be stored in the backend, and how the
+    //  data will be passed through to this component
+
+    return (
+        <div>
+            <h4>{props.name}</h4>
+            <p>Entered raffle 10/10/21</p>
+        </div>
+    )
+}
+
 
 Profile.propTypes = {
     auth: PropTypes.object.isRequired
