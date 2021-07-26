98833da3a327789a339501dfa7c0bbc38ce93c51
Drew Williamson <dreww@vt.edu>
2021-07-26 17:03:26 -0400

    Added skeletal design for 'Campain Crud' component (CRUD: Create Read Update Destroy if unaware).  Needs navigation included and buttons connected.

--- client/src/components/campaigns-crud/campaigns-crud.js
+++ client/src/components/campaigns-crud/campaigns-crud.js
@@ -2,9 +2,31 @@ import React from "react";
 import styles from "./campaigns-crud.module.css"
 
 function CampaignsCrud() {
-    return (
-        <div>
 
+    const locations = ['Restaurant 1', 'Restaurant 2'];
+    const campaigns = ['Campaign 1', 'Campaign 2', 'Campaign 3'];
+
+    return (
+        <div className={styles.container}>
+            {locations.map(i => {
+                return (
+                    <div style={{marginBottom: 20}}>
+                        <h2>{i}</h2>
+                        {campaigns.map(j => {
+                            return (
+                                <div style={{display: "flex", flexDirection: 'row'}}>
+                                    <h4 style={{marginLeft: 20}}>{j}</h4>
+                                    <div style={{flex: 1}} />
+                                    <i className="pencil alternate icon" style={{marginRight: 10}}/>
+                                    <i className='red trash icon' style={{marginRight: 30}} />
+                                    <p><u>View Results</u></p>
+                                </div>
+                            )
+                        })}
+                        <div style={{height: 1, width: '100%', background: "slategray", marginTop: 20}} />
+                    </div>
+                )
+            })}
         </div>
     )
 }
