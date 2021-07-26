f8acf33a63f70d7d386756c8c42cd00c92d62089
Drew Williamson <dreww@vt.edu>
2021-07-26 17:27:59 -0400

    Added 'coming soon' page for analytics.  Save analytics.js for the design

--- client/src/components/analytics/analytics.js
+++ client/src/components/analytics/analytics.js
@@ -3,8 +3,18 @@ import styles from "./analytics.module.css"
 
 function Analytics() {
     return (
-        <div>
-
+        <div className={styles.container}>
+            <i className='massive hourglass half icon'/>
+            <h1>Coming Soon!</h1>
+            <h4>
+                We're working hard to bring you new features to help you and your business.  Please check back here
+                in a few weeks to take advantage of the analytics we will compile from your patrons!
+            </h4>
+            <br/>
+            <div style={{textAlign: 'center', width: '90%'}}>
+                <h4 style={{margin: 0}}>Sincerely,</h4>
+                <h4 style={{margin: 0}}>Akinpoz</h4>
+            </div>
         </div>
     )
 }
