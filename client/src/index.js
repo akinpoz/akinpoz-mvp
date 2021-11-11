8c502ee20fe526ad64b6d3e647eca597579e72cd
Drew Williamson <dreww@vt.edu>
2021-11-11 15:51:13 -0500

    trying this out

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import axios from 'axios'
 
-axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:8001'
+axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? 'http://dev.apokoz.com' : 'http://localhost:8001'
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 ReactDOM.render(
