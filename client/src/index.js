d186bb233de289ac9c5674f8bf53563b888c10ae
Drew Williamson <dreww@vt.edu>
2021-11-15 13:33:47 -0500

    changed host

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import axios from 'axios'
 
-axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? 'http://dev.apokoz.com' : 'http://localhost:8001'
+const host = window.location.host
+axios.defaults.baseURL = host
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 ReactDOM.render(
